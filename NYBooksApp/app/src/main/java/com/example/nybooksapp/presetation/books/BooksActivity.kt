5ec1fa7eb8cda8d894354d3640ae0997698ef528
Ingrid Silva <ingridsilvatec@outlook.com>
2021-01-28 17:34:28 -0300

    implements data book, conclude adapter

--- NYBooksApp/app/src/main/java/com/example/nybooksapp/presetation/books/BooksActivity.kt
+++ NYBooksApp/app/src/main/java/com/example/nybooksapp/presetation/books/BooksActivity.kt
@@ -2,15 +2,16 @@ package com.example.nybooksapp.presetation.books
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nybooksapp.R
 import com.example.nybooksapp.databinding.ActivityBooksBinding
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = ActivityBooksBinding.inflate(layoutInflater)
+        val binding = ActivityBooksBinding.inflate(layoutInflater) //get layout
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(binding.root)  //set layout
 
-        binding.toolbarMain.title = "Books"
-        setSupportActionBar(binding.toolbarMain)
+        binding.toolbarMain.title = getString(R.string.books_title) //set title toolbar
+        setSupportActionBar(binding.toolbarMain) //set toolbar
     }
 }
---
+++ NYBooksApp/app/src/main/java/com/example/nybooksapp/presetation/books/BooksAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.nybooksapp.presetation.books
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nybooksapp.data.model.Book
+import com.example.nybooksapp.databinding.ItemBookBinding
+
+class BooksAdapter(
+    val books: List<Book>
+) : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        return BooksViewHolder(
+            ItemBookBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount() = books.count()
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        holder.bindView(books[position])
+    }
+
+    class BooksViewHolder(
+        val itemBookBinding: ItemBookBinding
+    ) : RecyclerView.ViewHolder(itemBookBinding.root) {
+        fun bindView(book: Book) {
+            itemBookBinding.title.text = book.title
+            itemBookBinding.author.text = book.author
+        }
+    }
+}
