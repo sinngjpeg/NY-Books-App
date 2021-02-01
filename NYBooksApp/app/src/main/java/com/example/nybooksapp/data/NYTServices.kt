0b0f099c487d04865d9fabdb902599809c427b3b
Ingrid Silva <ingridsilvatec@outlook.com>
2021-02-01 12:18:59 -0300

    create data class BookBodyResponse

--- NYBooksApp/app/src/main/java/com/example/nybooksapp/data/NYTServices.kt
+++ NYBooksApp/app/src/main/java/com/example/nybooksapp/data/NYTServices.kt
@@ -1,4 +1,13 @@
 package com.example.nybooksapp.data
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NYTServices {
+    @GET("lists.json")
+    fun getBooks(
+        @Query("api-key") apiKey: String = "SSLBb6NG0IVQw4GsWE3S3EPiXJ31O4mY",
+        @Query("list") list: String = "hardcover-fiction"
+    ): Call<List<>>
 }
---
+++ NYBooksApp/app/src/main/java/com/example/nybooksapp/data/response/BookBodyResponse.kt
@@ -0,0 +1,5 @@
+package com.example.nybooksapp.data.response
+
+data class BookBodyResponse(
+
+)
